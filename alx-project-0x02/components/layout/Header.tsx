059934ef0b08e8c95c2361f54cd451ebc2321c5e
gevalinho <gevalinho1@gmail.com>
2025-09-22 13:25:04 +0100

    2. Basic Routing in Next.js

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import Home from '@/pages'
+import About from '@/pages/about'
+import Link from 'next/link'
 function Header() {
   return (
-    <div>Header</div>
+    <>
+    <Link href="/home">Home</Link>
+    <Link href="/about">About</Link>
+    </>
   )
 }
 
