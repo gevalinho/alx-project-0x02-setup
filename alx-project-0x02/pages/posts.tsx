01c243f699c33e9a5797cc89fd32588ca763d992
gevalinho <gevalinho1@gmail.com>
2025-09-22 15:52:05 +0100

    8. Build a UserCard Component

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import PostCard from "@/components/common/PostCard";
 import { PostProps } from "@/interfaces";
-import Headers from "@/components/layout/Header";
+import Header from "@/components/layout/Header";
 import { type PostsPageProps } from "@/interfaces";
+
+ 
 
 
 
 
 const PostsPage: React.FC<PostsPageProps> = ({ posts }) => {
   return (
-    <><Headers />
+    <><Header />
     <div className="max-w-3xl mx-auto p-6">
       <h1 className="text-2xl font-bold mb-6">Posts</h1>
       {posts.map((post) => (
