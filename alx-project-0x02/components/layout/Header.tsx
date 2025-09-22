7f7612251db43f114157e540b0f96e37d817f404
gevalinho <gevalinho1@gmail.com>
2025-09-22 14:33:42 +0100

    4. Implement a Modal Component

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import Home from '@/pages'
-import About from '@/pages/about'
-import Link from 'next/link'
-function Header() {
+import Link from 'next/link'; 
+
+const Header: React.FC = () => {
   return (
-    <>
-    <Link href="/home">Home</Link>
-    <Link href="/about">About</Link>
-    </>
-  )
+    <header className="bg-blue-600 text-white shadow-md py-4">
+      <div className="container mx-auto flex justify-between items-center px-4">
+        <h3 className="font-bold text-2xl">
+     <Link href="/">Daily Contents</Link>
+  </h3>
+        <nav>
+          <ul className="flex space-x-6">
+            <li className="hover:underline">
+              <Link href="/home">Home</Link>
+            </li>
+            <li className="hover:underline">
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
